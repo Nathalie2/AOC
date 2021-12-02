9fc3612469a9772b8ba73458fd07c3f51b6b7b45
Nathalie2 <nathalieoomen@gmail.com>
2021-12-01 23:02:34 -0500

    Update main.py

--- Day1/main.py
+++ Day1/main.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#%% part 1
 import sys
 
 with open(sys.argv[1]) as f:
@@ -8,4 +9,7 @@ numbers = [int(line.strip()) for line in lines]
 
 count = sum(y>x for x,y in zip(numbers[:-1], numbers[1:]))
 
+print(count)
+#%% part 2
+count = sum(y>x for x,y in zip(numbers[:-3], numbers[3:]))
 print(count)
